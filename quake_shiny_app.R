0e3b7867bc7d690a0a04231e5ef86bb346019a7b
connermcb <connermcb@yahoo.com>
2018-04-01 15:05:20 -0500

    change rmd to r file

---
+++ quake_shiny_app.R
@@ -0,0 +1,165 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# get requisite packages
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(maps)
+library(cowplot)
+library(lubridate)
+library(shiny)
+library(sp)
+library(maptools)
+library(ptinpoly)
+
+get_quake_data <- function(output_format="csv",
+                           starttime=today()-180,
+                           endtime=today(),
+                           minlatitude=27,
+                           minlongitude=-103,
+                           maxlatitude=33,
+                           maxlongitude=-100,
+                           minmagnitude=2.5,
+                           maxmagnitude=10){
+  
+  # compose API request
+  base_url <- "https://earthquake.usgs.gov/fdsnws/event/1/query?"
+  
+  # add format arg
+  api_url <- paste0(base_url, "format=", output_format)
+  
+  # add start, end times
+  api_url <- paste0(api_url, "&starttime=", starttime)
+  api_url <- paste0(api_url, "&endtime=", endtime)
+  
+  # set longitude range
+  api_url <- paste0(api_url, "&minlongitude=", minlongitude)
+  api_url <- paste0(api_url, "&maxlongitude=", maxlongitude)
+  
+  # set latitude range
+  api_url <- paste0(api_url, "&minlatitude=", minlatitude)
+  api_url <- paste0(api_url, "&maxlatitude=", maxlatitude)
+  
+  # set magnitude range
+  api_url <- paste0(api_url, "&minmagnitude=", minmagnitude)
+  api_url <- paste0(api_url, "&maxmagnitude=", maxmagnitude)
+  
+  return(api_url)
+}
+
+test_pnt_in_ploy <- function(map_poly, pnt_df){
+
+  test_poly <- as.matrix(map_poly[ , 1:2])
+  test_coords <- as.matrix(pnt_df[, c("longitude", "latitude")])
+  View(class(test_coords))
+  idxs <- pip2d(test_poly, test_coords)
+  View(idxs)
+  new_df <- pnt_df[idxs, ]
+  return(new_df)
+}
+
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Earthquakes by State and Year"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         selectInput("year_adjust",
+                     "Select Year",
+                     choices = 2008:2018, 
+                     selected = NULL, 
+                     multiple = FALSE,
+                     selectize = TRUE, 
+                     width = NULL, 
+                     size = NULL),
+         
+         selectizeInput("state_adjust",
+                        "Select State",
+                        choices = state.name[!(state.name %in% 
+                                              c("Alaska", "Hawaii"))]),
+                        selected = "California",
+                        multiple = TRUE
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+   
+   output$distPlot <- renderPlot({
+     
+     # get base-map with UI input
+     base_map <- map_data("state", regions = tolower(input$state_adjust))
+     
+     # get bounding box
+     w_e <- range(base_map$long)
+     s_n <- range(base_map$lat)
+     
+     # scrape USGS data
+     source("C:\\Users\\conner\\daily_plots\\us_quakes_data\\searchComcat.R")
+     strt_time <- as.Date(paste(input$year_adjust, "01/01", sep = "/"))
+     end_time <- as.Date(paste(input$year_adjust, "12/31", sep = "/"))
+     data_url <-   get_quake_data(starttime = strt_time, 
+                                  endtime = min(today(), end_time),
+                                  minlatitude = s_n[1], maxlatitude = s_n[2],
+                                  minlongitude = w_e[1], maxlongitude = w_e[2],
+                                  minmagnitude = 2.5, maxmagnitude = 10)
+     quake_data <- read_csv(data_url)
+
+     quake_data2 <- test_pnt_in_ploy(base_map, quake_data)
+     
+  # get base map
+  base_map <- map_data("state", region = input$state_adjust)
+
+  # create plot
+  ggplot() +
+    geom_polygon(data=base_map,
+                 aes(x=long, y=lat, group=group),
+                 color="black", fill = "grey90", size = 1.5) +
+    geom_point(data=quake_data,
+               aes(x=longitude, y=latitude, color=mag),
+               alpha=0.8, size=4, shape=17) +
+    scale_color_continuous(name="Earthquake \nMagnitude",
+                           high = "yellow", low = "blue",
+                           limits = c(2.5, 5.5),
+                           breaks = seq(from=2.5, to=5.5, by=1),
+                           guide = guide_colorbar(barwidth = 1,
+                                                  barheight = 10,
+                                                  title.position = "left",
+                                                  label.vjust = 0.5)) +
+    ggtitle(input$year_adjust) +
+    # labs(caption = paste("Total Number of Earthquakes =",
+    #                      quake_data$cnt)) +
+    coord_map() +
+    theme_void() +
+    theme(plot.title = element_text(hjust = 0.5, size = 26),
+          plot.caption = element_text(hjust = 0, size=14),
+          legend.position = c(-0.25,-0.25),
+          legend.text = element_text(size=12),
+          legend.title = element_text(size=12))
+
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
